d7b8e8a727cb175b88499b2d99cccc517009ca9c
Koayniaina <setraniaina08@gmail.com>
2026-06-01 09:22:36 +0300

    resoudre mistake

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,6 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property bool $completed
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo whereCompleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Todo whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Todo extends Model
 {
     protected $fillable = [
